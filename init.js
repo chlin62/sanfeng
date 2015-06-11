029fb2a76cf89f1e54a7e682c2d8197be99b2ba0
chlin <s1012565@pu.edu.tw>
2015-06-11 11:23:51 +0800

    v1.0
    basic

---
+++ init.js
@@ -0,0 +1,10 @@
+$(function() {//Dom init
+			$(".button-collapse").sideNav();
+			$('.collapsible').collapsible({
+			accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+			});
+			$('.slider').slider({full_width: true});
+			$('.parallax').parallax();
+
+		});
+
